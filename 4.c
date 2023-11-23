6f3b867a55ddc908cd1573e661bd8a0a68063d2b
Dimpymittal22 <147979428+Dimpymittal22@users.noreply.github.com>
2023-11-23 09:35:45 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,14 @@
+//swapping without third variable
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d", &a);
+    printf("Enter the value of b:");
+    scanf("%d", &b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swapping: %d\n%d",a,b);
+}
