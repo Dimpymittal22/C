9a0ffae13c51c8b389bf50a99c34e4cdd29f1688
Dimpymittal22 <147979428+Dimpymittal22@users.noreply.github.com>
2023-11-23 08:56:12 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+//summation
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b;
+   printf("Enter the values of a and b");
+   scanf("%d%d", &a, &b);
+   printf("The sum is: %d",a+b);
+   return 0;
+}
