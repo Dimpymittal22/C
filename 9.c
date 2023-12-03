d8b181bc5c5932ba486eb476ea9e0fd66e4ae428
Dimpymittal22 <147979428+Dimpymittal22@users.noreply.github.com>
2023-12-03 16:03:10 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int area,b,h;
+    printf("Enter the values of breadth and height:");
+    scanf("%d%d", &b,&h);
+    area=0.5*b*h;
+    printf("The area of triangle is:%f",0.5*b*h);
+}
+    
