93987a12ff56d884d73818ae6ddafe115e096921
Dimpymittal22 <147979428+Dimpymittal22@users.noreply.github.com>
2023-12-03 16:08:34 +0530

    Add files via upload

---
+++ 16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the value of registration no.");
+    scanf("%d", &x);
+    if(x==2341001)
+    {
+        printf("Name-Dimpy Mittal\nTrade-GCS\nYear-2023");
+    }
+    if(x==2341002)
+    {
+        printf("Name-Sudipto\nTrade-GCS\nYear-2023");
+    }
+    if(x==2341003)
+    {
+        printf("Name-Akanksha Armo\nTrade-GCS\nYear-2023");
+    }
+    if(x==2341004)
+    {
+        printf("Name-Naresh\nTrade-GCS\nYear-2023");
+    }
+    if(x==2341005)
+    {
+        printf("Name-Gunjan\nTrade-GCS\nYear-2023");
+    }  
+}
