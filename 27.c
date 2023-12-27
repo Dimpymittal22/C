378ac595d70884d0a3116598b483900d2533c1e2
Dimpymittal22 <147979428+Dimpymittal22@users.noreply.github.com>
2023-12-27 10:49:52 +0530

    Add files via upload

---
+++ 27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0;
+    printf("Enter the number:");
+    scanf("%d", &n);
+    while(i<n)
+    {
+        printf("%d\n", i);
+        i=i+1; 
+    }
+}
